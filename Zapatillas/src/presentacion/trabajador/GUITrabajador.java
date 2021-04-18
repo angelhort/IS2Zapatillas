4294b2eda7c9253d612dd0e72bb7564c6068808d
ggasconn <guillermo.gasconsmr@gmail.com>
2021-04-18 13:32:57 +0200

    Added trabajdor GUI

---
+++ Zapatillas/src/presentacion/trabajador/GUITrabajador.java
@@ -0,0 +1,107 @@
+package presentacion.trabajador;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import presentacion.vista.ComponentsBuilder;
+import presentacion.vista.IGUI;
+
+public class GUITrabajador extends JFrame implements IGUI{
+	
+	private static final long serialVersionUID = 1L;
+
+	public GUITrabajador() {		
+		super("Foot World");
+		this.setBounds(100, 100, 1010, 500);
+		this.setVisible(true);
+		this.setLayout(null);
+	}
+	
+
+	public void initGUI() {
+		JLabel label = ComponentsBuilder.createLabel("Trabajador", 50, 30, 900, 50, Color.BLACK, new Font("Serif", Font.PLAIN, 30));
+		
+		this.add(label);
+		
+		JButton buttonAlta = ComponentsBuilder.createButton("Alta Trabajador", 100, 120, 185, 100, new Font("Serif", Font.PLAIN, 18));
+		this.add(buttonAlta);
+		
+		ActionListener lAlta = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+			
+		};
+		
+		buttonAlta.addActionListener(lAlta);
+		
+		JButton buttonBaja = ComponentsBuilder.createButton("Baja Trabajador", 407, 120, 185, 100, new Font("Serif", Font.PLAIN, 18));
+		this.add(buttonBaja);
+		
+		ActionListener lBaja = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+			
+		};
+		
+		buttonBaja.addActionListener(lBaja);
+		
+		JButton buttonModificar = ComponentsBuilder.createButton("Modificar Trabajador", 715, 120, 185, 100, new Font("Serif", Font.PLAIN, 18));
+		this.add(buttonModificar);
+		
+		ActionListener lModificar = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+			
+		};
+		
+		buttonModificar.addActionListener(lModificar);
+		
+		JButton buttonMostrarUno = ComponentsBuilder.createButton("Mostrar un Trabajador", 213, 290, 185, 100, new Font("Serif", Font.PLAIN, 18));
+		this.add(buttonMostrarUno);
+		
+		ActionListener lMostrarUno = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+			
+		};
+		
+		buttonMostrarUno.addActionListener(lMostrarUno);
+		
+		JButton buttonMostrarTodos = ComponentsBuilder.createButton("Mostrar todos los Trabajadores", 602, 290, 185, 100, new Font("Serif", Font.PLAIN, 14));
+		this.add(buttonMostrarTodos);
+		
+		ActionListener lMostrarTodos = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+			
+		};
+		
+		buttonMostrarTodos.addActionListener(lMostrarTodos);
+	}
+
+	@Override
+	public void actualizar() {
+		
+	}
+}
