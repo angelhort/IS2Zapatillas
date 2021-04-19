5be870b1bf0b3bec8b812373d26056828b214c41
angelhort <75445093+angelhort@users.noreply.github.com>
2021-04-19 18:25:01 +0200

    New main background

--- Zapatillas/src/presentacion/cliente/GUICliente.java
+++ Zapatillas/src/presentacion/cliente/GUICliente.java
@@ -23,7 +23,7 @@ public class GUICliente extends JFrame implements IGUI{
 		super("Foot World");
 		this.setBounds(100, 100, 1010, 500);
 		this.setVisible(true);
-		this.setContentPane(new JLabel(new ImageIcon("resources/Logo1.png")));
+		this.setContentPane(new JLabel(new ImageIcon("resources/1010x500.png")));
 		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
