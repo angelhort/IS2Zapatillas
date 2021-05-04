bc7b5c72064777d2a271efee528f77a5072820bc
angelhort <75445093+angelhort@users.noreply.github.com>
2021-05-04 10:12:38 +0200

    TransfersProducto

--- Zapatillas/src/negocio/producto/TransferCalcetines.java
+++ Zapatillas/src/negocio/producto/TransferCalcetines.java
@@ -1,82 +1,23 @@
 package negocio.producto;
 
-public class TransferCalcetines {
-
-	private int ID;
-	private int talla;
-	private double precio;
-	private String nombre;
-	private String color;
+public class TransferCalcetines extends TransferProducto{
+	
 	private String tejido;
-	private int stock;
-	private int almacen;
-	private int marca;
-	private boolean activo;
-	
-	public TransferCalcetines(int talla, double precio, String nombre, String color, 
-								String tejido, int stock, int almacen, int marca) {
-		this.talla = talla;
-		this.precio = precio;
-		this.nombre = nombre;
-		this.color = color;
+
+	public TransferCalcetines(int ID, int talla, double precio, String nombre, String color, String tejido, int stock,
+			int almacen, int marca, boolean activo) {
+		super(ID, talla, precio, nombre, color, stock, almacen, marca, activo);
 		this.tejido = tejido;
-		this.stock = stock;
-		this.almacen = almacen;
-		this.marca = marca;
+			
 	}
 	
-	public TransferCalcetines(int ID, int talla, double precio, String nombre, String color, 
-								String tipo, int stock, int almacen, int marca, boolean activo) {
-		this.ID = ID;
-		this.talla = talla;
-		this.precio = precio;
-		this.nombre = nombre;
-		this.color = color;
-		this.tejido = tipo;
-		this.stock = stock;
-		this.almacen = almacen;
-		this.marca = marca;
-		this.activo = activo;
-	}
-	
-	// GETTERS
-	public int getID() {
-		return ID;
-	}
-	
-	public int getTalla() {
-		return talla;
-	}
-	
-	public double getPrecio() {
-		return precio;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public String getColor() {
-		return color;
+	public TransferCalcetines(int talla, double precio, String nombre, String color, int stock,
+			int almacen, int marca, String tejido) {
+		super(talla, precio, nombre, color, stock, almacen, marca);
+		this.tejido = tejido;
 	}
 	
 	public String getTejido() {
 		return tejido;
-	}
-	
-	public int getStock() {
-		return stock;
-	}
-	
-	public int getAlmacen() {
-		return almacen;
-	}
-	
-	public int getMarca() {
-		return marca;
-	}
-	
-	public boolean getActivo() {
-		return activo;
 	}
 }
--- Zapatillas/src/negocio/producto/TransferZapatillas.java
+++ Zapatillas/src/negocio/producto/TransferZapatillas.java
@@ -1,82 +1,23 @@
 package negocio.producto;
 
-public class TransferZapatillas {
+public class TransferZapatillas extends TransferProducto{
 
-	private int ID;
-	private int talla;
-	private double precio;
-	private String nombre;
-	private String color;
 	private String tipo;
-	private int stock;
-	private int almacen;
-	private int marca;
-	private boolean activo;
-	
-	public TransferZapatillas(int talla, double precio, String nombre, String color, 
-								String tipo, int stock, int almacen, int marca) {
-		this.talla = talla;
-		this.precio = precio;
-		this.nombre = nombre;
-		this.color = color;
+
+	public TransferZapatillas(int ID, int talla, double precio, String nombre, String color, String tipo, int stock,
+			int almacen, int marca, boolean activo) {
+		super(ID, talla, precio, nombre, color, stock, almacen, marca, activo);
 		this.tipo = tipo;
-		this.stock = stock;
-		this.almacen = almacen;
-		this.marca = marca;
+			
 	}
 	
-	public TransferZapatillas(int ID, int talla, double precio, String nombre, String color, 
-								String tipo, int stock, int almacen, int marca, boolean activo) {
-		this.ID = ID;
-		this.talla = talla;
-		this.precio = precio;
-		this.nombre = nombre;
-		this.color = color;
+	public TransferZapatillas(int talla, double precio, String nombre, String color, String tipo, int stock,
+			int almacen, int marca) {
+		super(talla, precio, nombre, color, stock, almacen, marca);
 		this.tipo = tipo;
-		this.stock = stock;
-		this.almacen = almacen;
-		this.marca = marca;
-		this.activo = activo;
-	}
-	
-	// GETTERS
-	public int getID() {
-		return ID;
-	}
-	
-	public int getTalla() {
-		return talla;
-	}
-	
-	public double getPrecio() {
-		return precio;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public String getColor() {
-		return color;
 	}
 	
 	public String getTipo() {
 		return tipo;
-	}
-	
-	public int getStock() {
-		return stock;
-	}
-	
-	public int getAlmacen() {
-		return almacen;
-	}
-	
-	public int getMarca() {
-		return marca;
-	}
-	
-	public boolean getActivo() {
-		return activo;
 	}
 }
