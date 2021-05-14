1463d3bc82688aefcb241c753286b5d14746ac7e
ggasconn <guillermo.gasconsmr@gmail.com>
2021-05-14 19:46:33 +0200

    Finally f fixed db problems

--- Zapatillas/src/integracion/connection/DatabaseConnection.java
+++ Zapatillas/src/integracion/connection/DatabaseConnection.java
@@ -4,9 +4,12 @@ import java.sql.*;
 
 public class DatabaseConnection {
 	
-	public static final String DB_URL = "jdbc:mysql://db4free.net/zapatillas";
-	public static final String USERNAME = "zapatillas";
-	public static final String PASSWORD = "zapatillas";
+	//public static final String DB_URL = "jdbc:mysql://db4free.net/zapatillas";
+	//public static final String USERNAME = "zapatillas";
+	//public static final String PASSWORD = "zapatillas";
+	public static final String DB_URL = "jdbc:mysql://sql11.freemysqlhosting.net/sql11412469";
+	public static final String USERNAME = "sql11412469";
+	public static final String PASSWORD = "uW94nI67ke";
 	
 	public DatabaseConnection() {
 		// Empty
