9261e99eab4131a9049bf1840eabe3fbef2ad8ac
ggasconn <guillermo.gasconsmr@gmail.com>
2021-05-15 11:46:49 +0200

    Added attribute to transfer

--- Zapatillas/src/negocio/proveedor_producto/TransferProveedor_producto.java
+++ Zapatillas/src/negocio/proveedor_producto/TransferProveedor_producto.java
@@ -3,10 +3,12 @@ package negocio.proveedor_producto;
 public class TransferProveedor_producto {
 	private int idProveedor;
 	private int idProducto;
+	private double precioSuministro;
 	
-	public TransferProveedor_producto(int idProveedor, int idProducto){
+	public TransferProveedor_producto(int idProveedor, int idProducto, int precioSuministro){
 		this.idProveedor = idProveedor;
 		this.idProducto = idProducto;
+		this.precioSuministro = precioSuministro;
 	}
 
 	public int getIdProveedor() {
@@ -15,5 +17,9 @@ public class TransferProveedor_producto {
 
 	public int getIdProducto() {
 		return idProducto;
+	}
+	
+	public double getPrecioSuministro() {
+		return precioSuministro;
 	}
 }
