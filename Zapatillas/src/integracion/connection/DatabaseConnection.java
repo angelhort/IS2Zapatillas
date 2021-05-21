cc5224a553c628cd05919598579716b9952e090a
ggasconn <guillermo.gasconsmr@gmail.com>
2021-05-21 18:41:30 +0200

    New database credentials

--- Zapatillas/src/integracion/connection/DatabaseConnection.java
+++ Zapatillas/src/integracion/connection/DatabaseConnection.java
@@ -7,9 +7,9 @@ public class DatabaseConnection {
 	//public static final String DB_URL = "jdbc:mysql://db4free.net/zapatillas";
 	//public static final String USERNAME = "zapatillas";
 	//public static final String PASSWORD = "zapatillas";
-	public static final String DB_URL = "jdbc:mysql://sql11.freemysqlhosting.net/sql11412469";
-	public static final String USERNAME = "sql11412469";
-	public static final String PASSWORD = "uW94nI67ke";
+	public static final String DB_URL = "jdbc:mysql://sql11.freemysqlhosting.net/sql11413968";
+	public static final String USERNAME = "sql11413968";
+	public static final String PASSWORD = "jQAb62VXga";
 	
 	public DatabaseConnection() {
 		// Empty
